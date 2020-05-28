eec0954271601e433d74719558ff118e6659816c
a-kozhanov <a.kozhanov@outlook.com>
2020-05-29 00:57:46 +0300

    Added ErrorController

---
+++ WebStore/Controllers/ErrorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Error");
+        }
+
+        [Route("404")]
+        public IActionResult PageNotFound()
+        {
+            return View("_404");
+        }
+
+        [Route("{code:int}")]
+        public IActionResult Error(int code)
+        {
+            return View();
+        }
+
+        [Route("500")]
+        public IActionResult Error500()
+        {
+            return View("Error");
+        }
+    }
+}
